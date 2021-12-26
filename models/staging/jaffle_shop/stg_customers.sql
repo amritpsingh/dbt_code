be49698f1a45b1c845877010bb0f568d9bd63dea
Amrit <amritps28@gmail.com>
2021-12-26 07:15:14 +0000

    Update stg_customers and stg_orders

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,11 +1,5 @@
-with customers as (
-    
-    select
-        id as customer_id,
-        first_name,
-        last_name
-    from `dbt-tutorial-334816.bdt_dataset.Jaffle_shop_customers`
-
-)
-
-select * from customers
+select
+    id as customer_id,
+    first_name,
+    last_name
+from `dbt-tutorial-334816.bdt_dataset.Jaffle_shop_customers`
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,13 +1,7 @@
-with orders as (
+select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from `dbt-tutorial-334816.bdt_dataset.Jaffle_shop_orders`
-
-)
-
-select * from orders
+from `dbt-tutorial-334816.bdt_dataset.Jaffle_shop_orders`
